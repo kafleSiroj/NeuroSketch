506b3bf17dae44b668d2c56265b803c7b83789ec
kafleSiroj <sirojkafle0@gmail.com>
2026-05-21 20:38:01 +0545

    better file management i guess

---
+++ NeuroSketch/engine/module.py
@@ -0,0 +1,24 @@
+class Module:
+    def __init__(self):
+        self.params = {}
+        self.layers = []
+        self.out = None
+        self.input = None
+        self._backward = None
+
+    def forward(self, x):
+        raise NotImplementedError
+
+    def __call__(self, x):
+        return self.forward(x)
+    
+    def _register_params(self, weight, bias): 
+        if "weights" not in self.params:
+            self.params["weights"] = []
+            self.params["biases"] = []
+        
+        self.params["weights"].append(weight) 
+        self.params["biases"].append(bias)
+
+    def _backward(self):
+        raise NotImplementedError
