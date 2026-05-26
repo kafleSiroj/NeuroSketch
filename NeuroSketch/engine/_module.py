527dd972259b9bf7109fa5fe1735f315f1688c02
kafleSiroj <sirojkafle0@gmail.com>
2026-05-26 22:28:26 +0545

    good to go i think

--- NeuroSketch/engine/_module.py
+++ NeuroSketch/engine/_module.py
@@ -4,7 +4,7 @@ class Module:
         self.layers = []
         self.out = None
         self.input = None
-        self.grad_next = []
+        self.grad_next = None
 
     def forward(self, x):
         raise NotImplementedError
@@ -20,5 +20,5 @@ class Module:
         self.params["weights"].append(weight) 
         self.params["biases"].append(bias)
 
-    def backward_(self, grad_next):
+    def backward_(self, next_grad):
         raise NotImplementedError
